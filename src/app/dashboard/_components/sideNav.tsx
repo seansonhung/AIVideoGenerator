80b500ba8c12e256c045d628ef43a82915c1dc1f
seansonhung <seannguyen2919@gmail.com>
2024-11-24 23:12:39 -0500

    add dashboard layout and side nav component

---
+++ src/app/dashboard/_components/sideNav.tsx
@@ -0,0 +1,59 @@
+"use client"
+import React from 'react'
+import { CircleUser, PanelsTopLeft, FileVideo, SquarePlus, FolderClock, Menu } from 'lucide-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+export default function SideNav() {
+  const menuOptions = [
+    {
+      id:1,
+      name: 'Dashboard',
+      path: '/dashboard',
+      icon: PanelsTopLeft
+    },
+    {
+      id:2,
+      name: 'Create New',
+      path: '/create-new',
+      icon: FileVideo
+    },
+    {
+      id:3,
+      name: 'Account',
+      path: '/account',
+      icon: CircleUser
+    },
+    {
+      id:4,
+      name: 'Upgrade',
+      path: '/upgrade',
+      icon: SquarePlus
+    },
+    {
+      id:5,
+      name: 'History',
+      path: '/history',
+      icon: FolderClock
+    }
+  ]
+
+  const currentPath = usePathname();
+
+  return (
+    <div className='w-64 h-screen shadow-md p-5'>
+      <div className = 'gap-3'>
+        {menuOptions.map((item, index) => (
+          <Link href={item.path} key={index}>
+            <div className={`flex items-center gap-3 p-3 hover:bg-primary hover:text-white rounded-md cursor-pointer ${currentPath === item.path&&'bg-primary text-white'}`}>
+              <item.icon />
+              <h2> {item.name}
+                </h2>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from './_components/header'
+import SideNav from './_components/sideNav'
 
 export default function DashboardLayout({children}: {children: React.ReactNode}) {
   return (
     <div>
+      <div className='hidden md:block h-screen gb-white fixed mt-[65px] w-64'>
+        <SideNav />
+      </div>
       <div>
         <Header />
-        {children}
+        <div className='md: ml-64'>
+          {children}
+        </div>
       </div>
     </div>
   )
